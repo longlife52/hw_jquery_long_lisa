21bdef348f58d827e247ccf64fbc585d44388815
Lisa Long <garrisonridge@gmail.com>
2017-03-22 21:50:32 -0400

    Change last h2 to new copy via jQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 $(document).ready(function () {
 
+    var headings = $(' h2 ');
+    //$('h2').last().html('<h2>I am Lisa Long, and I am learning jQuery.</h2>');
+    $(headings).last().html('<h2>I am Lisa Long, and I am learning jQuery.</h2>');
 });
