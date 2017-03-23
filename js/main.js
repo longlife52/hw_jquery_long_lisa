26d6891d26dd425ad9678ab3245b54810fd96c7a
Lisa Long <garrisonridge@gmail.com>
2017-03-22 22:05:24 -0400

    Add paragraph style via jQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-
+    var paragraphs = $('p');
+    $(paragraphs).css({
+        'color': 'green',
+        'font-size': '0.625em',
+        'line-height': '2.5',
+        'padding-left': '2em'
+    });
 });
